88fcf0b349b6baff01b5be8a4dd3ec9b84f9c851
redizogolli <redi.zogolli@gmail.com>
2022-08-04 16:35:36 +0200

    StyleFixesMobile

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -9,6 +9,9 @@ function Movies() {
   const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.currentGenreOrCategory);
   const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
 
+  const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
+  const numberOfMovies = lg ? 16 : 18;
+
   const changePage = (event, value) => {
     setPage(value);
   };
@@ -45,7 +48,7 @@ function Movies() {
   }
   return (
     <div>
-      <MovieList movies={data} />
+      <MovieList movies={data} numberOfMovies={numberOfMovies} />
       <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages > 500 ? 500 : data.total_pages} />
       {/* <Pagination count={data.total_pages > 500 ? 500 : data.total_pages} page={page} onChange={changePage} /> */}
     </div>
