c8741dba8a5ac2f77729e01f19b3f083a7084626
redizogolli <redi.zogolli@gmail.com>
2022-08-09 14:56:57 +0200

    AlanAi- login &logout

--- src/components/AlanAI/AlanAI.jsx
+++ src/components/AlanAI/AlanAI.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { ColorModeContext } from '../../utils/ToogleColorMode';
+import { fetchToken } from '../../utils';
+import { unsetUser } from '../../features/auth';
 
 const useAlanAI = () => {
   const { setMode } = useContext(ColorModeContext);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   useEffect(() => {
     alanBtn({
       key: process.env.REACT_APP_ALANAI_SKD_KEY,
       onCommand: ({ command, mode }) => {
-        console.log('Command', command);
         if (command === 'changeMode') {
           console.log('Changing mode to: ', mode);
           if (mode === 'light') {
@@ -16,6 +23,13 @@ const useAlanAI = () => {
           } else {
             setMode('dark');
           }
+        } else if (command === 'login') {
+          fetchToken();
+        } else if (command === 'logout') {
+          // clear the state
+          dispatch(unsetUser());
+          // redirect to home
+          navigate('/');
         }
       },
     });
